e1dd17ec5a0c55f5b4cf98d3c1c003bf126dc8ec
李瑞东 <1058892959@qq.com>
2020-08-16 19:41:12 +0800

    代码提交

--- Week_08/homework/homework.js
+++ Week_08/homework/homework.js
@@ -63,4 +63,57 @@ function merge(intervals) {
     return result;
 }
 
-console.log(merge([[1,4],[0,0]]));
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function reversePairs(nums) {
+    if(!nums || nums.length < 1) return 0;
+
+    //归并排序
+    let count = 0;
+
+    function _mergeSort(arr) {
+
+        function _sort(begin, end){
+            if(begin >= end) return ;
+            let middle = (begin + end) >> 1;
+
+            _sort(begin, middle);
+            _sort(middle + 1, end);
+            _merge(begin, middle, end);
+        }
+
+        function _merge(begin, middle, end) {
+            //合并 begin->middle middle+1->end
+            let left = begin,  right = middle+1, start = 0, temp = [];
+
+            while(right <= end) {
+                let _left = begin;
+                while (_left <= middle) {
+                    if(arr[_left] >> 1 > arr[right]) {
+                        count++;
+                    }
+                    _left++;
+                }
+                while (left <= middle && arr[left] < arr[right]) temp[start++] = arr[left++];
+                temp[start++] = arr[right++];
+            }
+
+            while (left <= middle) {
+                temp[start++] = arr[left++];
+            }
+
+            //合并
+            arr.splice(begin, end -begin + 1, ...temp);
+        }
+
+        _sort(0, arr.length - 1);
+
+        return arr;
+    }
+
+    _mergeSort(nums);
+    return count;
+}
+
